d91df4951b1c3039dbec35a496c1f16e5f79d491
Adam Ras <adamras1@wp.pl>
2017-04-02 20:05:52 +0200

    repositories for entities

---
+++ Angular2WebApi/Template.DAL/Repositories/BookTypeTypeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Commons.DTOs;
+using Commons.Entities;
+using Template.DAL.BaseRepository;
+
+namespace Template.DAL.Repositories
+{
+    public class BookTypeTypeRepository : BaseRepository<BookType, BookTypeDto>
+    {
+        public BookTypeTypeRepository(DatabaseContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Angular2WebApi/Template.DAL/Repositories/PublishingHouseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Commons.DTOs;
+using Commons.Entities;
+using Template.DAL.BaseRepository;
+
+namespace Template.DAL.Repositories
+{
+    public class PublishingHouseRepository : BaseRepository<PublishingHouse, PublishingHouseDto>
+    {
+        public PublishingHouseRepository(DatabaseContext context) : base(context)
+        {
+        }
+    }
+}
